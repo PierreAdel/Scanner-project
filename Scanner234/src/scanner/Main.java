c996d1b098f4690ceef11c10d61a755ec9706091
PierreAdel <38634825+PierreAdel@users.noreply.github.com>
2020-04-02 03:08:47 +0200

    Update Main.java

--- Scanner234/src/scanner/Main.java
+++ Scanner234/src/scanner/Main.java
@@ -4,7 +4,18 @@ import java.util.Scanner;
 import java.util.*; 
 
 public class Main {
+    /*private boolean eq(String)
+    {
+    
+    
+    }*/
     public static void main(String[] args) {
+        
+           
+        
+        
+        
+        
         Scanner obj = new Scanner(System.in);
         String string= "null";
         System.out.println("Enter your code in TINY language to be scanned");
@@ -16,11 +27,15 @@ public class Main {
             charArray[i] = string.charAt(i); 
         } 
         // m3aya character array el user mda5alha esmaha charArray
+        Token token;
         Code_Scanner scanner = new Code_Scanner(charArray);
-        while(scanner.pointer<=string.length()){
-            
-            Token token = scanner.tokenize();
+        while(scanner.pointer<string.length()){ // removed <= and solved index out of bound  
+             
+            token = scanner.tokenize();
+           
+         //   System.out.print(scanner.pointer);
             token.printTokenName();
+            
         }
         
         
@@ -28,3 +43,6 @@ public class Main {
     }
     
 }
+
+
+
